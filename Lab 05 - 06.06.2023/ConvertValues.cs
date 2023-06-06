c8974d1e26d48db85823e1250df1d5cf9d1d3fd7
Isira Adithya <rp.isira@gmail.com>
2023-06-06 09:58:57 +0530

    Added Kilometer to Meter Cod

---
+++ Lab 05 - 06.06.2023/ConvertValues.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23052023___Lab_Session
+{
+    public class ConvertValues
+    {
+        public void kilometerToMeterNoReturnNoParameter()
+        {
+            Console.Write("Enter the kilometer value: ");
+            double kmValue = double.Parse(Console.ReadLine());
+            Console.WriteLine(kmValue + " km = " + (kmValue * 1000) + " m");
+
+        }
+
+        public void kilometerToMeterNoReturn(double kmValue)
+        {
+            Console.WriteLine(kmValue + " km = " + (kmValue * 1000) + " m");
+        }
+
+        public double kilometerToMeter(double kmValue)
+        {
+            Console.WriteLine(kmValue + " km = " + (kmValue * 1000) + " m");
+            return kmValue * 1000;
+        }
+    }
+}
